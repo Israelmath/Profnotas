408a1c8006140f46eba401cb609dcf2191744ce2
Israelmath <israel.usp@gmail.com>
2020-07-27 12:23:41 -0300

    Excluindo arquivos desnecessários

--- Screens/adicionaAlunoTela.py
+++ Screens/adicionaAlunoTela.py
@@ -32,3 +32,43 @@ class AdicionaAlunoTela(Screen):
 
     def limpa_info(self, *args):
         self.ids.verificacao.text = ''
+
+    def get_info_aluno(self, text, nome, *args):
+        ids_tela = self.ids
+        not_check = ['form', 'verificacao', 'btn_config']
+        show_btn_confirm = True
+        if nome == 'Nome':
+            ids_tela.show_NomeSobrenome.text = text.title()
+        if nome == 'Sobrenome':
+            if ids_tela.show_NomeSobrenome.text != '':
+                ids_tela.show_NomeSobrenome.text = ids_tela.show_NomeSobrenome.text
+        if nome == 'Numero':
+            ids_tela.show_Numero.text = text.title()
+        if nome == 'Serie':
+            ids_tela.show_Serie.text = text.title()
+        if nome == 'Turma':
+            ids_tela.show_Turma.text = text.title()
+
+        for id in ids_tela:
+            if id not in not_check:
+                if ids_tela[id].text == '':
+                    show_btn_confirm = False
+
+        self.change_btn_confirm(show_btn_confirm)
+
+    def change_btn_confirm(self, show_btn):
+        id_btn = self.ids.btn_config
+        if show_btn:
+            id_btn.padding = (0, 50)
+            id_btn.size_hint = (None, None)
+            id_btn.size = (200, 60)
+            id_btn.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+            id_btn.border = (0, 0, 0, 0)
+            id_btn.text = 'Confirmar inscrição'
+            id_btn.font_name = 'Fonts/AmaticSC-Bold.ttf'
+            id_btn.color = (0, 0, 0, 1)
+            id_btn.font_size = 24
+            id_btn.background_color = (1, 1, 1, 1)
+        else:
+            id_btn.background_normal = ''
+            id_btn.background_color = (0, 0, 0, 0)
